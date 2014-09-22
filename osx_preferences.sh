9b06303d7b828fe8dc3233097666109f80f87acc
Stephen Holtz <git@holtz.email>
2014-09-22 00:03:40 -0400

    initial commit

---
+++ osx_preferences.sh
@@ -0,0 +1,202 @@
+#!/usr/bin/env bash
+# Based on the incredible https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# For documentation http://www.defaults-write.com
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing sudo time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Menu bar: disable transparency
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable App Nap
+defaults write NSGlobalDomain NSAppSleepDisabled -bool YES
+
+# Disable Resume system-wide
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# Disable automatic termination of inactive apps
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze on
+
+# SSD-specific 
+
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
+
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
+
+# Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+
+# Keyboard trackpad
+
+# Stop iTunes from responding to the keyboard media keys
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# finder
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Empty Trash securely by default
+defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Dock desktop etc.,
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Enable spring loading for all Dock items
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Wipe all (default) app icons from the Dock
+defaults write com.apple.dock persistent-apps -array ""
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Don’t group windows by application in Mission Control
+# (i.e. use the old Exposé behavior instead)
+defaults write com.apple.dock expose-group-by-app -bool false
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock more transparent
+defaults write com.apple.dock hide-mirror -bool true
+
+# Time Machine
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+# Activity Monitor
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+# Enable the debug menu in Disk Utility
+defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+# Kill affected applications
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+	"Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
+	"Terminal" "iCal"; do
+	killall "${app}" > /dev/null 2>&1
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
---
+++ provision_mac.sh
@@ -0,0 +1,186 @@
+#!/bin/sh
+# need xcode command-line/developer tools 
+# Xcode 5.1+, OSX 10.9+ xcode-select --install
+
+# Colorize+format output
+function better_echo () {
+    NORMAL=$(tput sgr0)
+    GREEN=$(tput setaf 40)
+    printf "\n%b\n" "${GREEN}$1${NORMAL}"
+}
+
+better_echo "Starting mac provisioning script!"
+
+# Set zsh as SHELL
+if grep -qE 'zsh' "$SHELL"; then
+    better_echo "SHELL already set to zsh."
+else
+    better_echo "Setting SHELL to zsh."
+    chsh -s $(which zsh) 
+fi 
+
+# Install homebrew
+if hash brew &> /dev/null; then
+    better_echo "Homebrew found, skipping installation."
+else
+    better_echo "Installing homebrew."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update / clean before installs
+better_echo "Updating and upgrading homebrew."
+brew update && brew upgrade && brew cleanup
+
+# Add taps to brew before install
+better_echo "Adding taps to homebrew."
+taplist=$(brew tap);
+# cask required for app installs!
+if [[ ! $taplist =~ 'caskroom/cask' ]]; then
+    brew tap caskroom/cask
+fi
+# very useful science utils
+if [[ ! $taplist =~ 'homebrew/science' ]]; then
+    brew tap homebrew/science
+fi
+# backup for pip
+if [[ ! $taplist =~ 'homebrew/python' ]]; then
+    brew tap homebrew/python
+fi
+brew tap --repair
+
+# install utilities with brew
+better_echo "Installing homebrew recipes from brew.installs."
+brewlist=$(brew list)
+while read -r line; do
+    if [[ $line != \#* ]]; then 
+        if [[ ! $brewlist =~ $line ]]; then
+            better_echo "Installing $line"
+            brew install $line
+        else
+            better_echo "$line already found in brew list"
+        fi
+    fi
+done < brew.installs
+
+# install free/freemium apps with brew-cask 
+better_echo "Installing apps with homebrew-cask from cask.installs."
+if brewlist | grep -q 'brew-cask'; then
+    better_echo "Found brew-cask."
+else
+    better_echo "Did not find brew-cask, installing now."
+    brew install brew-cask
+fi 
+brew cask cleanup
+
+casklist=$(brew cask list)
+while read -r line; do
+    if [[ $line != \#* ]]; then 
+        if [[ ! $casklist =~ $line ]]; then
+            better_echo "Installing $line"
+            brew cask install $line
+        else
+            better_echo "$line already found in brew list"
+        fi
+    fi
+done < cask.installs 
+
+# make sure alfred app can get to caskroom directory
+better_echo "Attempting to link Alfred"
+brew cask alfred status
+if [ $? -ne 0 ]; then 
+    brew cask alfred link
+fi
+
+better_echo "Tidying and linking homebrew"
+brew cleanup
+brew cask cleanup
+brew linkapps
+
+# gems configuration -- use rbenv ruby-build
+# insert shims in the path to # not muck with shipped
+# ruby requires rbenv, rbenv-build, rbenv-gem-rehash
+better_echo "Configuring ruby environment"
+
+PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
+rbenv install 2.1.2
+rbenv global 2.1.2
+gem update --system
+gem install tmuxinator lolcat json json_pure vimgolf
+gem install bundler foreman pg rails thin --no-rdoc --no-ri
+
+# python -- use Anaconda for data analysis
+better_echo "Configuring Anaconda installation"
+if ! hash conda &> /dev/null; then
+    EX_DIR=$(pwd)
+    mkdir "$HOME/Downloads/Provisioning"
+    cd "$HOME/Downloads/Provisioning"
+    ANACONDA_VER='2.0.1'
+    ANACONDA_MD5='85d261fd4e651f891ca5d0df69441e00'
+    better_echo "Downloading Anaconda installer to " $HOME/Downloads/Provisioning/
+    curl -O --progress-bar http://repo.continuum.io/archive/Anaconda-$ANACONDA_VER-MacOSX-x86_64.sh
+    if [[ ! $(md5 -q Anaconda-$ANACONDA_VER-MacOSX-x86_64.sh) =~ $ANACONDA_MD5 ]]; then
+        better_echo 'Anaconda download failed md5.'
+    else
+        better_echo "Installing Anaconda $ANACONDA_VER"
+        bash Anaconda-$ANACONDA_VER-MacOSX-x86_64.sh
+    fi
+    rm -f Anaconda-$ANACONDA_VER-MacOSX-x86_64.sh &> /dev/null
+    cd $EX_DIR
+else
+    better_echo "Anaconda already installed"
+fi
+better_echo "Updating Anaconda packages"
+conda update conda
+conda update anaconda
+
+# other python tools
+better_echo "Configuring pip install packanges"
+pip install git+git://github.com/Lokaltog/powerline
+pip install awscli
+pip install setuptools
+pip install termdown
+
+# R
+better_echo "Configuring R environment"
+# TODO
+# node 
+better_echo "Configuring node environment"
+# TODO
+
+# Now set up osx preferences
+better_echo "Configuring OSX Preferences, need to log out and in for some to take effect."
+bash osx_prefs.sh
+dockutil --add '~/Downloads' --view grid --display automatic
+
+# Clone/build code repositories
+better_echo "Configuring code repositories"
+# Git global config (.gitconfig in dotfiles)
+git config --global user.name Stephen Holtz 
+git config --global user.email git@holtz.email 
+# git-credential-osxkeychain default installs with brew install git
+git config --global credential.helper osxkeychain
+# line ending preferences
+git config --global core.autocrlf input
+git config --global core.safecrlf true
+
+## dotfile setup (includes antigen subrepository)
+if [ ! -d "$HOME/dotfiles" ]; then
+    git clone https://github.com/stephenholtz/dotfiles.git $HOME/dotfiles
+fi
+cd $HOME/dotfiles; sh $HOME/dotfiles/linkall.sh
+
+if [ ! -d "$HOME/code" ]; then
+    mkdir -v $HOME/code
+fi
+if [ ! -d "$HOME/code/grad" ]; then
+    mkdir -v $HOME/code/grad/
+    git clone https://github.com/stephenholtz/matlab-utils.git $HOME/code/grad/matlab-utils
+    git clone https://github.com/stephenholtz/matlab-tiff-utils.git $HOME/code/grad/matlab-tiff-utils
+fi
+if [ ! -d "$HOME/code/personal" ]; then
+    mkdir -v $HOME/code/personal/
+fi
+
+better_echo "Script finished. Log out and in"
+better_echo "iTerm2.app not yet configured"
