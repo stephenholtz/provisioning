1b338bec3ea0d9be6ad68a232b5b4a9af855eaad
Stephen Holtz <git@holtz.email>
2014-10-15 13:01:45 -0400

    add win7 rig provisioning

---
+++ provision_ephys_win7.bat
@@ -0,0 +1,44 @@
+rem Download this script and run as adminstrator 
+rem
+rem For ephys workstation:
+rem Not Installed: Matlab, FlyCap2, Office 2013 Pro Plus, Nidaq tools
+
+rem install chocolatey
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy unrestricted SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+rem packages:
+rem choco install package-name == cinst package-name
+
+rem reqd
+cinst javaruntime
+
+rem languages
+cinst python
+
+rem version control
+cinst git.install
+cinst git.commandline
+cinst svn
+cinst hg
+
+rem utilities
+cinst Ghostscript
+cinst putty.portable
+cinst autohotkey.portable
+cinst 7zip.commandline
+cinst 7zip.install
+cinst imagemagick
+cinst dropbox
+
+rem applications
+cinst VirtualCloneDrive
+cinst GoogleChrome
+cinst vlc
+cinst greenshot -Version 1.1.9.13
+cinst MicrosoftSecurityEssentials
+cinst flashplayeractivex
+cinst notepadplusplus
+cinst teamviewer
+cinst windirstat
+cinst lastpass
+cinst synergy
